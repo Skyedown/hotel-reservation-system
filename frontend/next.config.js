c62df8e35fa9fe91154651c40a62cc9ed9d63fb8
Skyedown <lehocky.peter@yahoo.com>
2025-08-13 06:45:19 +0200

    fixed prod security settings

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -20,7 +20,7 @@ const cspHeader = `
     child-src 'self';
     manifest-src 'self';
     navigate-to 'self' https://checkout.stripe.com https://js.stripe.com;
-    trusted-types hotel-system-policy default 'nextjs#bundler'${isDevelopment ? ' \'unsafe-inline\'' : ''};
+    trusted-types hotel-system-policy default ${isDevelopment ? '\'nextjs#bundler\' \'unsafe-inline\'' : '\'nextjs#bundler\' \'unsafe-inline\''};
     ${isDevelopment ? '/* require-trusted-types-for \'script\'; */' : '/* require-trusted-types-for \'script\'; */'}
     upgrade-insecure-requests;
 `
